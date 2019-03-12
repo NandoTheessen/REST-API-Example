2aa78c42ec691b277858691355e736db89f462ae
Nando Theessen <nando@buzzbird.de>
2019-03-12 09:09:43 +0100

    add go.sum & setup basic webserver serving locall

--- cmd/apiv1-local/main.go
+++ cmd/apiv1-local/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/go-chi/chi"
 	"github.com/sirupsen/logrus"
 )
@@ -9,4 +11,6 @@ func main() {
 	log := logrus.New()
 	router := chi.NewRouter()
 
+	log.Println("Serving on :8080. Example here: http://localhost:8080/")
+	log.Fatal(http.ListenAndServe(":8080", router), nil)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
-module "github.com/nandotheessen//REST-API-Example"
+module github.com/nandotheessen/REST-API-Example
 
 go 1.12
+
+require (
+	github.com/go-chi/chi v4.0.1+incompatible
+	github.com/sirupsen/logrus v1.3.0
+)
