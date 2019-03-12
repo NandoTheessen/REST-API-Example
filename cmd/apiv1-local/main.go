cdf46a8a988376f6f7fb532c5a0a3643c9deb73e
Nando Theessen <nando@buzzbird.de>
2019-03-12 08:59:45 +0100

    Initial setup for server & folder structure

---
+++ cmd/apiv1-local/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	log := logrus.New()
+	router := chi.NewRouter()
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module "github.com/nandotheessen//REST-API-Example"
+
+go 1.12
