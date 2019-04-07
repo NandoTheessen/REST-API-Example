a9ca9e540ac4ab01dc7e6eaf498eb7670f90065b
Nando Theessen <nando@buzzbird.de>
2019-04-07 13:32:20 +0200

    feat(handlers): add router instantiation & first handler

---
+++ internal/handlers/router.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+
+	"github.com/buzzbird/go-service-helpers/pkg/output"
+	"github.com/sirupsen/logrus"
+)
+
+type handlerShared struct {
+	log *logrus.Logger
+}
+
+func (h *handlerShared) helloWorld(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	output.WriteResponse(ctx, h.log, w, http.StatusOK, "Hello World!")
+}
+
+func NewRouter(log *logrus.Logger) *chi.Mux {
+	h := &handlerShared{
+		log: log,
+	}
+
+	r := chi.NewRouter()
+
+	r.Route("/", func(r chi.Router) {
+		r.Get("/", h.helloWorld)
+	})
+
+	return r
+}
