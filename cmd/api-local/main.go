3e2635f5c138775e09d4f79dd9d59031b5de21aa
Nando Theessen <nando@buzzbird.de>
2019-04-07 13:31:28 +0200

    fix(repo): correct file names, initial server setup

--- cmd/api-local/main.go
+++ cmd/api-local/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"net/http"
 
-	"github.com/go-chi/chi"
+	"github.com/nandotheessen/REST-API-Example/internal/handlers"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	log := logrus.New()
-	router := chi.NewRouter()
+	router := handlers.NewRouter(log)
 
 	log.Println("Serving on :8080. Example here: http://localhost:8080/")
 	log.Fatal(http.ListenAndServe(":8080", router), nil)
